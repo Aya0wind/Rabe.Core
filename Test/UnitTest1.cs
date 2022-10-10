c22f64e37c9fc68a93bb1f5bb6e76d2263cd69a7
Aya0wind <120035959@qq.com>
2022-10-10 22:30:12 +0800

    Fixed: wrongly escaped json quote in json serialization

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using Rabe;
 using NUnit.Framework;
 
@@ -22,21 +23,13 @@ public class Tests
     [Test]
     public void CPABETest()
     {
-        Rabe.CPABE.SecretKey secretKey = Rabe.CPABE.Extension.KeyGen(_masterKey,new string[]{"A"});
-        var secretKeyJson = JsonSerializer.Serialize(secretKey, new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true,
-        });
+        Rabe.CPABE.SecretKey secretKey = Rabe.CPABE.Extension.KeyGen(_masterKey,new[]{"A", "B"});
+        var secretKeyJson = JsonSerializer.Serialize(secretKey);
         Console.WriteLine(secretKeyJson);
         var secretKeyDeserialized = JsonSerializer.Deserialize<Rabe.CPABE.SecretKey>(secretKeyJson);
-        var message = Encoding.Default.GetBytes("Hello world!");
-        var cipher = Rabe.CPABE.Extension.Encrypt(_publicKey,"\"A\" or \"B\"", message );
-        var cipherJson = JsonSerializer.Serialize(cipher, new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true,
-        });
+        var message = Encoding.Default.GetBytes("dsafsadsa");
+        var cipher = Rabe.CPABE.Extension.Encrypt(_publicKey,"\"A\" and \"B\"", message );
+        var cipherJson = JsonSerializer.Serialize(cipher);
         Console.WriteLine(cipherJson);
         var cipherDeserialized = JsonSerializer.Deserialize<Rabe.CPABE.Cipher>(cipherJson);
         var text = secretKeyDeserialized.Decrypt(cipherDeserialized);
@@ -47,20 +40,12 @@ public class Tests
     public void KPABETest()
     {
         Rabe.KPABE.SecretKey secretKey = Rabe.KPABE.Extension.KeyGen(_masterKey,"\"A\" and \"B\"");
-        var secretKeyJson = JsonSerializer.Serialize(secretKey, new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true,
-        });
+        var secretKeyJson = JsonSerializer.Serialize(secretKey);
         Console.WriteLine(secretKeyJson);
         var secretKeyDeserialized = JsonSerializer.Deserialize<Rabe.KPABE.SecretKey>(secretKeyJson);
-        var message = Encoding.Default.GetBytes("Hello world!");
-        var cipher = Rabe.KPABE.Extension.Encrypt(_publicKey,new string[]{"A", "B"}, message );
-        var cipherJson = JsonSerializer.Serialize(cipher, new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true,
-        });
+        var message = Encoding.Default.GetBytes("dsafsadsa");
+        var cipher = Rabe.KPABE.Extension.Encrypt(_publicKey,new[]{"A", "B"}, message );
+        var cipherJson = JsonSerializer.Serialize(cipher);
         Console.WriteLine(cipherJson);
         var cipherDeserialized = JsonSerializer.Deserialize<Rabe.KPABE.Cipher>(cipherJson);
         var text = secretKeyDeserialized.Decrypt(cipherDeserialized);
